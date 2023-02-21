500cb20eceb90eff1909dcd5b02b86d2758edfc1
Matt Miller <mattmillerf@pm.me>
2023-02-20 20:04:17 -0500

    added default logic for no arg

--- src/main.rs
+++ src/main.rs
@@ -3,28 +3,42 @@ use clap::Parser;
 #[derive(Parser)]
 struct CLI {
     string: String,
-    rot: usize 
+    rot: Option<usize>
 }
 
-fn main() {
-    let args = CLI::parse();
+fn rot(s: String, r: usize) -> String {
     let alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
     let upper_alphabet = alphabet
         .iter()
         .map(|c| c.to_ascii_uppercase())
         .collect::<Vec<_>>();
-    let n = args.rot;
-    let rot = args
-        .string
+
+    let rot = s
         .chars()
         .map(|c| *alphabet.iter()
              .chain(alphabet.iter())
              .chain(upper_alphabet.iter())
              .chain(upper_alphabet.iter())
              .skip_while(|&x| *x != c)
-             .nth(n)
+             .nth(r)
              .unwrap_or(&c)
-             )
+            )
         .collect::<String>();
-    println!("{:?}", rot);
+
+    return rot;
+}
+
+fn main() {
+    let args = CLI::parse();
+    
+    match args.rot {
+    
+        Some(n) => {
+            println!("{:?}", rot(args.string, n));
+        },
+        None => {
+            println!("{:?}", rot(args.string, 13));
+        }
+
+    }
 }
