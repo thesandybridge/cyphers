390ed39d6ceff4041decd7b77da6e34dfd838e5d
Matt Miller <mattmillerf@pm.me>
2023-03-03 00:22:18 -0500

    change names, add tests

---
+++ src/lib.rs
@@ -0,0 +1,39 @@
+/// Rot cyhpher algorithm.
+///
+/// # Arguments
+///
+/// * `s` - String to rotate.
+/// * `r` - Rotation amount for the cypher.
+///
+/// # Examples
+///
+/// ```
+/// let rot13 = cypher::rot(String::from("abc"), 13);
+/// // test for standard rotation of 13 characters.
+/// assert_eq!(rot13, String::from("nop"));
+///
+/// let custom = cypher::rot(String::from("abc"), 1);
+/// // test for custom rotation.
+/// assert_eq!(custom, String::from("bcd"));
+/// ```
+pub fn rot(s: String, r: usize) -> String {
+    let alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    let upper_alphabet = alphabet
+        .iter()
+        .map(|c| c.to_ascii_uppercase())
+        .collect::<Vec<_>>();
+
+    let rot = s
+        .chars()
+        .map(|c| *alphabet.iter()
+             .chain(alphabet.iter())
+             .chain(upper_alphabet.iter())
+             .chain(upper_alphabet.iter())
+             .skip_while(|&x| *x != c)
+             .nth(r)
+             .unwrap_or(&c)
+            )
+        .collect::<String>();
+
+    return rot;
+}
--- src/main.rs
+++ src/main.rs
@@ -1,58 +1,17 @@
 use clap::Parser;
 
-#[derive(Parser)]
-struct CLI {
+#[derive(Parser, Debug)]
+struct Args {
+    /// String to rotate.
+    #[arg()]
     string: String,
-    rot: Option<usize>
+
+    /// Rotation amount for the cypher.
+    #[arg(default_value_t = 13)]
+    rot: usize
 }
-
-fn rot(s: String, r: usize) -> String {
-    let alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-    let upper_alphabet = alphabet
-        .iter()
-        .map(|c| c.to_ascii_uppercase())
-        .collect::<Vec<_>>();
-
-    let rot = s
-        .chars()
-        .map(|c| *alphabet.iter()
-             .chain(alphabet.iter())
-             .chain(upper_alphabet.iter())
-             .chain(upper_alphabet.iter())
-             .skip_while(|&x| *x != c)
-             .nth(r)
-             .unwrap_or(&c)
-            )
-        .collect::<String>();
-
-    return rot;
-}
-
-#[cfg(test)]
-mod rot {
-    // Note this useful idiom: importing names from outer (for mod tests) scope.
-    use super::*;
-
-    #[test]
-    fn base_case_test() {
-        assert_eq!(rot(String::from("abc"), 13), String::from("nop"));
-    }
-
-    #[test]
-    fn custom_rotation_test() {
-        assert_eq!(rot(String::from("abc"), 1), String::from("bcd"));
-    }
-
-}
-
 
 fn main() {
-    let args = CLI::parse();
-    
-    if let Some(n) = args.rot {
-        println!("{:?}", rot(args.string, n));
-    } else {
-        println!("{:?}", rot(args.string, 13));
-    }
-
+    let args = Args::parse();
+    println!("{:?}", cypher::rot(args.string, args.rot));
 }
