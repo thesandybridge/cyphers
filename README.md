96ca8509404d2abd8b80678038bea3e85d59dbef
Matt Miller <mattmillerf@pm.me>
2023-03-03 00:49:51 -0500

    convert rot to sub command

--- README.md
+++ README.md
@@ -5,6 +5,6 @@ A rot cypher CLI built with rust. Build the binary and execute.
 - accepts two arguments, string and number to rotate by.
 
 ```shell
-$ ./rot "Hello, world!" 13 
+$ ./cypher "Hello, world!"
 "Uryyb, Jbeyq!"
 ```
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,25 @@
-use clap::Parser;
+use clap::{arg, Command};
 
-#[derive(Parser, Debug)]
-struct Args {
-    /// String to rotate.
-    #[arg()]
-    string: String,
-
-    /// Rotation amount for the cypher.
-    #[arg(default_value_t = 13)]
-    rot: usize
+fn cli() -> Command {
+    Command::new("cypher")
+        .about("A bunch of cypher tools.")
+        .subcommand_required(true)
+        .subcommand(
+            Command::new("rot")
+                .arg(arg!(<STRING> "String to rotate").required(true))
+                .arg(arg!(<ROT> "Rotation amount").required(false).default_value("13").value_parser(clap::value_parser!(usize)))
+        )
 }
 
 fn main() {
-    let args = Args::parse();
-    println!("{:?}", cypher::rot(args.string, args.rot));
+    let commands = cli().get_matches();
+
+    match commands.subcommand() {
+        Some(("rot", sub)) => {
+            let string = sub.get_one::<String>("STRING").unwrap();
+            let rot = sub.get_one::<usize>("ROT").unwrap();
+            println!("{:?}", cypher::rot(string.to_string(), *rot));
+        }
+        _ => println!("Invalid command. Please use <help> to see full list of commands.")
+    }
 }
